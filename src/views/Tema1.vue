6d0397088ee0bf41dd826dceed50c2bf32c1f9c7
veimarcm <vcelis@sena.edu.co>
2023-12-18 08:55:45 -0500

    Video CF1
    vIDEO

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -403,7 +403,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video          
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/WTJGT2cMkQI" title="Métricas del marketing digital" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen)
     Separador
     .row
       .col-xl-12
